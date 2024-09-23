54f276323e24e6290b8f1db5ed1f6bc6b12a3778
dauletle <dauletle@gmail.com>
2024-09-22 18:31:05 -0700

    Removed speed_factor, added tracker and debug.

--- helium3.py
+++ helium3.py
@@ -1,34 +1,40 @@
 import simpy
 
-import utils.util_cli as util_cli
+import utils.cli as cli
 from unload_station import UnloadStation
 from truck import Truck
 from mine import Mine
 from lunar_mine_operator import LunarMineOperator
-
-SIMULATION_TIME_HOURS = 72 * 60
+from utils.tracker import Tracker
 
 def __main__():
-    number_of_mining_trucks, number_of_unload_stations, speed_factor = util_cli.get_arguments()
+    args = cli.get_arguments()
+    number_of_mining_trucks = args.n
+    number_of_unload_stations = args.m
+    simulation_time_hours = args.t
+    debug = args.debug
 
     # Initialize SimPy environment
     env = simpy.Environment()
 
+    # Initialize tracker
+    tracker = Tracker()
+
     # Assume infinite mining sites (so no resources needed here)
-    mine = Mine(env, speed_factor)
+    mine = Mine(env, debug)
     
     # Create the number of unload_stations.Unloadstation objects specified in number_of_unload_stations
     unload_stations = []
     for i in range(number_of_unload_stations):
         uld_stn = UnloadStation(name="UnloadStation{}".format(i + 1),
                                 env=env,
-                                speed_factor=speed_factor)
+                                tracker=tracker,
+                                debug=debug,
+                                )
         unload_stations.append(uld_stn)
     
     # Lunar Mine Operator manages routing to unload stations
-    lunar_mine_operator = LunarMineOperator(env, 
-                                            unload_stations,
-                                            speed_factor)
+    lunar_mine_operator = LunarMineOperator(env, unload_stations, debug)
     
     # Create the number of mining_trucks.Truck objects specified in number_of_mining_trucks
     mining_trucks = []
@@ -37,11 +43,16 @@ def __main__():
                     env=env,
                     mine=mine,
                     operator=lunar_mine_operator,
-                    speed_factor=speed_factor)
+                    tracker=tracker,
+                    debug=debug,
+                    )
         mining_trucks.append(trk)
     
     # Run the simulation
-    env.run(until=SIMULATION_TIME_HOURS/speed_factor)
+    env.run(until=simulation_time_hours)
+
+    # Print the simulation statistics
+    tracker.report()
 
 
 if __name__ == "__main__":
