0d2275c473b245bfb352ee21784abfd09052d606
dauletle <dauletle@gmail.com>
2024-09-22 19:09:26 -0700

    Adding leading zeros to names.

--- helium3.py
+++ helium3.py
@@ -26,7 +26,8 @@ def __main__():
     # Create the number of unload_stations.Unloadstation objects specified in number_of_unload_stations
     unload_stations = []
     for i in range(number_of_unload_stations):
-        uld_stn = UnloadStation(name="UnloadStation{}".format(i + 1),
+        station_name = "UnloadStation{:0{width}}".format(i + 1, width=len(str(number_of_unload_stations)))
+        uld_stn = UnloadStation(name=station_name,
                                 env=env,
                                 tracker=tracker,
                                 debug=debug,
@@ -39,7 +40,8 @@ def __main__():
     # Create the number of mining_trucks.Truck objects specified in number_of_mining_trucks
     mining_trucks = []
     for i in range(number_of_mining_trucks):
-        trk = Truck(name="Truck{}".format(i + 1),
+        truck_name = "Truck{:0{width}}".format(i + 1, width=len(str(number_of_mining_trucks)))
+        trk = Truck(name=truck_name,
                     env=env,
                     mine=mine,
                     operator=lunar_mine_operator,
